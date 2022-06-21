113a74e55dca401068d0857c905f5d5de3c3a19c
Rafael Ortolani <rafael.ortolani@senior.com.br>
2022-06-21 09:29:25 -0300

    basic auth

--- src/main/java/com/exemplo/config/SecurityConfig.java
+++ src/main/java/com/exemplo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.exemplo.config;
 import com.exemplo.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,6 +33,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .hasRole("ADMIN")
                 .antMatchers("/api/pedidos/**")
                 .hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "api/usuarios/**")
+                .permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
